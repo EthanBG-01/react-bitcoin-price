ef9304b59813df1fcfbb10f4b05fef98fefe876b
Ethan <63881996+EthanDevs404@users.noreply.github.com>
2021-11-10 10:45:58 +0000

    basic site

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import Axios from "axios";
+import React, { useState } from "react";
+import { Button } from '@material-ui/core';
 
 function App() {
+
+    const [price, setPrice] = useState();
+
+    async function getPrice () {
+        console.log("HERE");
+        try {
+            const result = await Axios.get("https://api.coinbase.com/v2/prices/spot?currency=USD");
+            console.log(result);
+            setPrice(result.data.data.amount);
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+          <h1>Get Latest Bitcoin Price</h1>
+          <Button color="primary" onClick={() => { getPrice()}}>Get Price</Button>
+          {
+              price ? <h2>${price}</h2> : <br/>
+          }
+      </div>
   );
 }
 
